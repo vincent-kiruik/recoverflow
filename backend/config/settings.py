5e91872c51b0671d206e28d7f4fda0da45593fb4
vincekiru <vkirui7878@gmail.com>
2026-05-28 15:04:37 +0300

    settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,7 +104,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
-
+CORS_ALLOW_CREDENTIALS = True
 # Celery
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
